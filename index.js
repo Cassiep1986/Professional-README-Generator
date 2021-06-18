d3607a38b8f4f8da7274536e4df78df469798860
seacrest <cassiep1986@gmail.com>
2021-06-17 17:13:18 -0700

    Added 1st questions

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-// TODO: Include packages needed for this application
+const questions = require('inquirer');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+questions 
+.prompt ([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+      },
+      {
+        type: 'input',
+        name: 'Description',
+        message: 'Enter a description of your project',
+      },
+      {
+        type: 'checkbox',
+        name: 'Contents',
+        message: 'Check all applicable table of contents',
+        choices: ["Description", "Usage", ]
+      },
+    ])
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+.then((data) => {
+    const mdPageContent = generateMarkdown(data);
+    fs.writeFile('index.html', mdPageContent, (err) =>
+    err ? console.log(err) : console.log('Successfully created md.html!')
+  );
+});
 // TODO: Create a function to initialize app
 function init() {}
 
